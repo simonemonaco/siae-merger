f9fe5cdadeeff2c1aa7446b59ec612641f894930
simone7monaco <simone7monaco@gmail.com>
2023-12-17 14:28:02 +0100

    Fix on old reports

--- siae_merger.py
+++ siae_merger.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
+import re
 
-st.header('SIAEE Merger')
-upload_files = st.file_uploader('', accept_multiple_files=True, type=['csv'])
+st.header('SIAE Merger')
+upload_files = st.file_uploader('', accept_multiple_files=True, type=['csv', 'xlsx'])
 css='''
 <style>
 [data-testid="stFileUploadDropzone"] div div::before {content:"Trascina i file da unire qui"}
 [data-testid="stFileUploadDropzone"] div div span{display:none;}
-[data-testid="stFileUploadDropzone"] div div::after {font-size: .8em; content:"Limite 200Mb per file • CSV"}
+[data-testid="stFileUploadDropzone"] div div::after {font-size: .8em; content:"Limite 200Mb per file • CSV/XLSX"}
 [data-testid="stFileUploadDropzone"] div div small{display:none;}
 </style>
 '''
@@ -16,9 +17,18 @@ st.markdown(css, unsafe_allow_html=True)
 	
 full = None
 for rep in upload_files:
-	report = pd.read_csv(rep, sep=';', header=4)
-	report = report[report['CODICE OPERA'] != 'TOTALE']
+	if rep.name.lower().endswith('csv'):
+		report = pd.read_csv(rep, sep=';', header=4)
+	elif rep.name.lower().endswith('xlsx'):
+		report = pd.read_excel(rep, header=4, dtype=str, engine='openpyxl').rename(columns={
+									'TITOLO OPERA': 'TITOLO OPERE', 
+								    'CLASSE': 'CLASSE DI RIPARTIZIONE', 
+									'MATURATO': f'MATURATO {anno} - {semestre}'})
+		report = report[report['CODICE OPERA'].notna()]
+	report = report[(report['CODICE OPERA'] != 'TOTALE') & (report['CLASSE DI RIPARTIZIONE'] != 'TOTALE')]
 	report = report.set_index('CODICE OPERA')
+
+	anno, semestre = re.findall(r'(\d{4})[-_](\d)', rep.name)[0][:2]
 	if full is None:
 		full = report
 	else:
@@ -27,7 +37,7 @@ for rep in upload_files:
 		full['CLASSE DI RIPARTIZIONE'] = full[['CLASSE DI RIPARTIZIONE', 'CLASSE DI RIPARTIZIONE_new']].apply(lambda x: x[0] if not isinstance(x[0], float) else x[1], axis=1)
 
 if full is not None:
-	full = full[['TITOLO OPERE', 'CLASSE DI RIPARTIZIONE'] + sorted([c for c in full.columns if c.startswith('MATURATO')])]
+	full = full[['TITOLO OPERE', 'CLASSE DI RIPARTIZIONE'] + sorted([c for c in full.columns if c.startswith('MATURATO')])].drop_duplicates()
 
 	st.write('Scarica il file')
 	st.download_button('Download', key='dl', data=pd.DataFrame.to_csv(full, encoding='utf-8'), file_name='SIAEE.csv', mime='text/csv')
