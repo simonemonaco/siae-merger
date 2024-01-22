479dda47862e8943c2018da4715ff1374feca1f7
simone7monaco <simone7monaco@gmail.com>
2024-01-22 22:15:16 +0100

    Fix on random number of initial rows

--- siae_merger.py
+++ siae_merger.py
@@ -18,9 +18,16 @@ st.markdown(css, unsafe_allow_html=True)
 full = None
 for rep in upload_files:
 	if rep.name.lower().endswith('csv'):
-		report = pd.read_csv(rep, sep=';', header=4, decimal=',', thousands='.')
+		report = pd.read_csv(rep, sep=';', decimal=',', thousands='.', header=None, names=['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10'])
 	elif rep.name.lower().endswith('xlsx'):
-		report = pd.read_excel(rep, header=4, dtype=str, engine='openpyxl')
+		report = pd.read_excel(rep, header=None, dtype=str, engine='openpyxl', names=['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13', 'c14', 'c15'], index_col=None)
+	
+	report = report.dropna(axis=1, how='all')
+	report = report.loc[report[report.c1.isin(['CODICE OPERA', 'CLASSE'])].index[0]:]
+	report.columns = report.iloc[0]
+	report = report[1:].reset_index(drop=True)
+
+	if rep.name.lower().endswith('xlsx'):
 		report['MATURATO'] = report['MATURATO'].astype(float)
 		
 	anno, semestre = re.findall(r'(\d{4})[-_](\d)', rep.name)[0][:2]
